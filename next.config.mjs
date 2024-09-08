68b73559379e39d1e47fbf66e7776c2649d62304
codewithxavi <javieradrian.it@gmail.com>
2024-09-08 13:23:02 +0200

    Fix errors

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   assetPrefix: isProd ? "/code_notecode/" : "",
-  basePath: isProd ? "/code_notecode" : "",
+  basePath: isProd ? "/code_notecode/" : "",
   trailingSlash: true,
   output: "export",
   experimental: {
