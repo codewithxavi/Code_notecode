e1c615fe539dc9206cefdfb378414396a278c9de
codewithxavi <javieradrian.it@gmail.com>
2024-09-08 13:07:01 +0200

    Fix errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/code_notecode",
+  assetPrefix: "/code_notecode/",
+  trailingSlash: true,
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
