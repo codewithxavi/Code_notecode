5631e62e736a13da51877e2a8272710d84a9f4ba
codewithxavi <javieradrian.it@gmail.com>
2024-09-08 13:49:41 +0200

    Fix errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  basePath: "/code_notecode",
-  assetPrefix: "/code_notecode/", // assetPrefix requires the trailing slash
+  assetPrefix: isProd ? "/code_notecode/" : "",
+  basePath: isProd ? "/code_notecode" : "",
   trailingSlash: true,
   output: "export",
   experimental: {
